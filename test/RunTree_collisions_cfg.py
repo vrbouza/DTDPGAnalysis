83432a565db30d4cfdf53d029758321c4013205f
Victor Rodriguez Bouza <vrbouza@outlook.com>
2018-12-07 23:21:38 +0100

    To run simulations from 9_3_7

--- test/RunTree_collisions_cfg.py
+++ test/RunTree_collisions_cfg.py
@@ -32,7 +32,9 @@ process.load("RecoMuon.TrackingTools.MuonServiceProxy_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")  #DB v2, at least since GR_E_V42
 
-process.GlobalTag.globaltag = '94X_dataRun2_ReReco_EOY17_v2'
+#process.GlobalTag.globaltag = '94X_dataRun2_ReReco_EOY17_v2'
+#process.GlobalTag.globaltag =  '93X_upgrade2023_realistic_v5'
+process.GlobalTag.globaltag =  '101X_upgrade2018_realistic_v7'
 
 # for the emulator
 process.load("L1TriggerConfig.DTTPGConfigProducers.L1DTTPGConfigFromDB_cff")
@@ -46,26 +48,27 @@ process.load('RecoLuminosity.LumiProducer.lumiProducer_cfi')
 # process.load('EventFilter.L1TRawToDigi.l1tRawtoDigiBMTF_cfi')
 process.load('EventFilter.L1TRawToDigi.bmtfDigis_cfi')
 
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
 
   fileNames = cms.untracked.vstring
   (
     #'/store/data/Run2017F/SingleMuon/RAW-RECO/ZMu-17Nov2017-v1/00000/06FB9E1B-BEF1-E711-9122-0025905A608E.root',    # La vaina de antes
-    '/store/mc/PhaseIIFall17D/SingleMu_FlatPt-2to100/GEN-SIM-DIGI-RAW/L1TnoPU_93X_upgrade2023_realistic_v5-v1/00000/F4EEAE55-C937-E811-8C29-48FD8EE739D1.root', # La vaina loca nueva
-
+    #'/store/mc/PhaseIIFall17D/SingleMu_FlatPt-2to100/GEN-SIM-DIGI-RAW/L1TnoPU_93X_upgrade2023_realistic_v5-v1/00000/F4EEAE55-C937-E811-8C29-48FD8EE739D1.root', # La vaina loca nueva
+    #'/store/relval/CMSSW_10_1_1/RelValSingleMuPt100/GEN-SIM-RECO/101X_upgrade2018_realistic_v7-v1/10000/24374C57-533C-E811-9F00-003048FFD71C.root' # Nuevina, de CMSSW_10_1_1
+    '/store/relval/CMSSW_9_3_7/RelValSingleMuPt100/GEN-SIM-RECO/PU25ns_93X_upgrade2023_realistic_v5_2023D17PU70-v1/20000/FE28DACA-1F52-E811-B471-0242AC130002.root' # Una de reco de 9_3_7
   ),
   secondaryFileNames = cms.untracked.vstring(
   ),
   inputCommands = cms.untracked.vstring(
-        'keep *',
-        'drop l1tEMTFHit2016Extras_simEmtfDigis_CSC_HLT',
-        'drop l1tEMTFHit2016Extras_simEmtfDigis_RPC_HLT',
-        'drop l1tEMTFHit2016s_simEmtfDigis__HLT',
-        'drop l1tEMTFTrack2016Extras_simEmtfDigis__HLT',
-        'drop l1tEMTFTrack2016s_simEmtfDigis__HLT'
+       'keep *',
+       'drop l1tEMTFHit2016Extras_simEmtfDigis_CSC_HLT',
+       'drop l1tEMTFHit2016Extras_simEmtfDigis_RPC_HLT',
+       'drop l1tEMTFHit2016s_simEmtfDigis__HLT',
+       'drop l1tEMTFTrack2016Extras_simEmtfDigis__HLT',
+       'drop l1tEMTFTrack2016s_simEmtfDigis__HLT'
   )
 )
 
@@ -119,16 +122,24 @@ process.rpcRecHits.rpcDigiLabel = cms.InputTag('rpcUnpackingModule')
 
 
 # process.p = cms.Path(process.DTMuonSelection * process.dtunpacker * process.twinMuxStage2Digis  * process.scalersRawToDigi * process.lumiProducer * process.dtTriggerPrimitiveDigis + process.BMTFStage2Digis + process.rpcUnpackingModule + process.rpcRecHits + process.myDTNtuple)
+
 process.p = cms.Path(process.DTMuonSelection * process.dtunpacker * process.twinMuxStage2Digis  * process.scalersRawToDigi * process.lumiProducer * process.dtTriggerPrimitiveDigis + process.bmtfDigis + process.rpcUnpackingModule + process.rpcRecHits + process.myDTNtuple)
 # Output
+
 process.out = cms.OutputModule("PoolOutputModule"
                                , outputCommands = cms.untracked.vstring(
                                											"keep *",
                                                                          "keep *_*_*_testRPCTwinMuxRawToDigi"
                                                                        , "keep *_*_*_DTNTandRPC"
 																		)
-#                                , fileName = cms.untracked.string("file:cia.root")
+                               , fileName = cms.untracked.string("file:DTNtuple.root")
                                , SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring("p"))
 )
 
-
+process.fanout = cms.EndPath(
+    process.out
+)
+process.schedule = cms.Schedule(
+    process.p,
+    process.fanout
+)
